2ae768b8fad87554d30838c2b97112a8b4c1bc05
Shady <Shadymtadros@hotmail.com>
2019-11-27 15:03:53 +0100

    done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,27 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(rest_param)
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render :new
+    end
+  end
+
+  private
+  def rest_param
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
